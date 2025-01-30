0faf3fd325c11a55ec60964d6445859fcc19287d
ZahraKAN <zahrakintani05@gmail.com>
2025-01-30 22:06:09 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,72 @@
+gsap.from(".name", { 
+    opacity: 0, 
+    scale: 0.5, 
+    duration: 1.5, 
+    ease: "elastic.out(1, 0.5)"
+});
 
+gsap.from(".welcome", { 
+    opacity: 0, 
+    scale: 0.5, 
+    duration: 1.5, 
+    ease: "elastic.out(1, 0.5)"
+});
+
+gsap.from(".img-profile", { 
+    opacity: 0, 
+    scale: 0.5, 
+    duration: 1.5, 
+    ease: "elastic.out(1, 0.5)"
+});
+
+gsap.from(".quote", {
+    opacity: 0, 
+    x: -50, 
+    duration: 2.5, 
+    delay: 0.5, 
+    ease: "power2.out"
+});
+
+gsap.from(".img-profile2", {
+    opacity: 0,
+    x: -100,
+    duration: 1.5,
+    ease: "power3.out"
+});
+
+gsap.from(".about h1", {
+    opacity: 0,
+    y: 50,
+    duration: 1.5,
+    ease: "power3.out"
+});
+
+gsap.from(".about p, .about ul", {
+    opacity: 0,
+    y: 20,
+    duration: 1,
+    stagger: 0.3, 
+    ease: "power2.out"
+});
+
+gsap.from(".Project", { 
+    opacity: 0, 
+    y: -50, 
+    duration: 1.2, 
+    ease: "back.out(1.7)" 
+});
+
+gsap.from(".logo", { 
+    opacity: 0, 
+    y: -50, 
+    duration: 1.2, 
+    ease: "back.out(1.7)", 
+    stagger: 0.2 
+});
+
+gsap.from(".title", { 
+    opacity: 0, 
+    scale: 0.5, 
+    duration: 1.5, 
+    ease: "elastic.out(1, 0.5)"
+});
